bc785fd78c4774cc68c92ff45d55e3261ec5c690
shazy89 <erdo.shazy123@gmail.com>
2021-07-26 18:01:06 +0200

    use the getStaticPage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import fs from "fs/promises";
+import path from "path";
 const HomePage = () => {
   return (
     <ul>
@@ -10,4 +10,14 @@ const HomePage = () => {
   );
 };
 
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products
+    }
+  };
+}
 export default HomePage;
