635b0250c90abf955e2a10a42b328e893d7e13ae
shazy89 <erdo.shazy123@gmail.com>
2021-07-27 15:46:18 +0200

    swr next js custom fetching hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,35 +1,59 @@
 import { useEffect, useState } from "react";
-
+import useSWR from "swr";
 const LastSales = () => {
-  const [sales, setSales] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [sales, setSales] = useState();
+  // const [loading, setLoading] = useState(false);
+  const { data, error } = useSWR(
+    "https://practice-next-250c4-default-rtdb.firebaseio.com/sales.json"
+  );
   useEffect(() => {
-    setLoading(true);
-    fetch("https://practice-next-250c4-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (let key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume
-          });
-        }
-        setSales(transformedSales);
-        setLoading(false);
-      }); // add json at the end
-  }, []);
-  if (loading) {
-    return <h4>Loading .....</h4>;
+    if (data) {
+      const transformedSales = [];
+      for (let key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch("https://practice-next-250c4-default-rtdb.firebaseio.com/sales.json") // add json at the end
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       const transformedSales = [];
+  //       for (let key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load</p>;
   }
-  console.log(sales);
-  const array = sales.map((sale) => (
-    <li key={sale.id}>
-      {sale.username} - ${sale.volume}
-    </li>
-  ));
-  return <ul>{array}</ul>;
+  if (!sales || !data) {
+    return <p>Loadin ....</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default LastSales;
