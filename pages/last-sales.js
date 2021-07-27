98e5cf1b5c217d51c24da5c3e06809e380f26ccf
shazy89 <erdo.shazy123@gmail.com>
2021-07-27 08:19:46 +0200

    displaying the data from firebase

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
 
 const LastSales = () => {
+  const [sales, setSales] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch("https://practice-next-250c4-default-rtdb.firebaseio.com/sales.json")
       .then((response) => response.json())
-      .then((data) => {}); // add json at the end
+      .then((data) => {
+        const transformedSales = [];
+        for (let key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume
+          });
+        }
+        setSales(transformedSales);
+        setLoading(false);
+      }); // add json at the end
   }, []);
-  return (
-    <ul>
-      <h1>Hi There</h1>
-    </ul>
-  );
+  if (loading) {
+    return <h4>Loading .....</h4>;
+  }
+  console.log(sales);
+  const array = sales.map((sale) => (
+    <li key={sale.id}>
+      {sale.username} - ${sale.volume}
+    </li>
+  ));
+  return <ul>{array}</ul>;
 };
 
 export default LastSales;
